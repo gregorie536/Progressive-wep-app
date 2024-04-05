5d540b67cd9a861a9b35bba322a75d446fdb4ab8
gregorie536 <vexier.gregorie@gmail.com>
2024-04-05 10:32:22 +0200

    save

--- main.js
+++ main.js
@@ -35,3 +35,28 @@ saveFileButton.addEventListener('click', async () => {
     await handle.writableStream.close();
     quill.setText();
 })
+
+////////////////////////////////////////////////////////////////////
+
+async function fetchWeather(city) {
+    const apiKey = '1be4254a0d10ee3db39453a87d010519';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        const cityElement = document.getElementById('city');
+        const temperatureElement = document.getElementById('temperature');
+        const descriptionElement = document.getElementById('description');
+
+        cityElement.textContent = data.name;
+        temperatureElement.textContent = `Température : ${data.main.temp}°C`;
+        descriptionElement.textContent = data.weather[0].description;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données météo", error);
+    }
+}
+
+fetchWeather("Paris");
